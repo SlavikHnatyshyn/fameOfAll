da54eb35a5b717a91469388c70184d5a03491993
Slavik <hnatyshuns@gmail.com>
2018-07-16 11:55:30 +0300

    fixed

--- src/modules/getList.js
+++ src/modules/getList.js
@@ -1,24 +1,20 @@
-import { HallOfFame } from 'baseClass';
+import { HallOfFame } from './baseClass';
 
 
 HallOfFame.prototype.getlist = function () {
 
-           
     let res = [];
     this.sort();
 
-    // здесь в новый массив res мы скопируем 'свойство:значение' из массива players
+
     res = [].concat(this.players).map(a => `${a[0]}: ${a[1]}`);
     console.log(res);
 
-    // если длина массива больше допустимого количества значения (this.size), то мы возвращаем массив, где скопируем элементы до допустимого значения     
     if (res.length >= this.size) {
         return res.slice(0, this.size);
-
-
     }
 
-    // вычисляем сколько значений мы еще можем заполнить, и отправляем на их место пустую строку
+
     else {
 
         res.push.apply(res, Array.from(new Array(this.size - res.length), () => ''))
@@ -28,4 +24,4 @@ HallOfFame.prototype.getlist = function () {
 
 };
 
-export {getlist};
+export { getlist };
--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ module.exports = {
         filename: './dist/bundle.js'
     },
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/
-            }
-        ]
+        // loaders: [
+        //     {
+        //         test: /\.js$/,
+        //         exclude: /node_modules/
+        //     }
+        // ]
     }
 };
